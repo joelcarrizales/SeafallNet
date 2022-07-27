f6c2b15e447e7f1c61960bc67b2aeba1df608c11
Joel Carrizales <joelcarrizales22@gmail.com>
2022-07-27 09:20:27 -0500

    Adding player and advisor data. Adding game controller. Adding route. Refactoring to reduce tabbing

--- Seafall/Seafall/ClientApp/src/setupProxy.js
+++ Seafall/Seafall/ClientApp/src/setupProxy.js
@@ -5,7 +5,8 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:26465';
 
 const context =  [
-  "/weatherforecast",
+    "/game",
+    "/weatherforecast"
 ];
 
 module.exports = function(app) {
---
+++ Seafall/Seafall/Controllers/GameController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Seafall.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GameController : ControllerBase
+{
+    private readonly ILogger<GameController> _logger;
+
+    public GameController(ILogger<GameController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public string Get()
+    {
+        string x = System.IO.File.ReadAllText(@"ClientApp\data.json");
+
+        return System.IO.File.ReadAllText(@"ClientApp\data.json");
+    }
+}
