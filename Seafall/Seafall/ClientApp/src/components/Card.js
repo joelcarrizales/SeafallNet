4620f1b36da8847214bd2d7aaeaa29e68d4663b2
Joel Carrizales <joelcarrizales22@gmail.com>
2022-07-27 21:41:40 -0500

    Creating card component

---
+++ Seafall/Seafall/ClientApp/src/components/Card.js
@@ -0,0 +1,46 @@
+﻿import React, { Component } from 'react';
+
+export default class Card extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            advisor: this.props.advisor
+            //{ name: "", epithet: "", ability: "", bonus1: "", bonus2: "", bonus3: "", repCost: 0, goldCost: 0,guild: "",set: 0,currentOwner: 0,status: "Available" }
+        }
+    }
+
+    render() {
+        return (
+            <div className="card">
+                <div className="name">
+                    {this.state.advisor.name}
+                </div>
+                <div className="epithet">
+                    {this.state.advisor.epithet}
+                </div>
+                <p>
+                    {this.state.advisor.ability}
+                </p>
+                <div className="bonuses">
+                    <span>
+                        {this.state.advisor.bonus1}
+                    </span>
+                    <br />
+                    <span>
+                        {this.state.advisor.bonus2}
+                    </span>
+                    <br />
+                    <span>
+                        {this.state.advisor.bonus3}
+                    </span>
+                </div>
+                <div className="costs">
+                    {this.state.advisor.repCost}
+                    <br/>
+                    {this.state.advisor.goldCost}
+                </div>
+            </div>
+        );
+    }
+}
--- Seafall/Seafall/ClientApp/src/components/Home.js
+++ Seafall/Seafall/ClientApp/src/components/Home.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
+import Card from './Card';
 
 export class Home extends Component {
     static displayName = Home.name;
 
     constructor(props) {
         super(props);
-        this.state = { game: "advisors NONE", loading: true };
+        this.state = { game: { advisors :[] }, loading: true };
     }
     componentDidMount() {
        // this.populateGameData();
     }
 
-  render(game) {
-    return (
-        <div>
+    render() {
+        let contents = this.state.loading
+        ? <p><em>Loading...</em></p>
+        : <Card advisor={this.state.game.advisors[0]} />
+
+        return (
+            <div>
             <input type="file" id="gameFile" onChange={this.handleFileChange} />
             <h1>Hello, world!</h1>
-            { this.state.game }
-      </div>
-    );
+            { contents }
+            </div>
+        );
     }
 
     async populateGameData() {
@@ -31,8 +36,7 @@ export class Home extends Component {
         let input = event.target.files[0];
         let reader = new FileReader();
         reader.onloadend = (event) => {
-            this.setState({ game: event.target.result, loading: false });
-            //this.setState({ game: JSON.parse(event.target.result), loading: false });
+            this.setState({ game: JSON.parse(event.target.result), loading: false });
         };
         reader.readAsText(input);
     };
