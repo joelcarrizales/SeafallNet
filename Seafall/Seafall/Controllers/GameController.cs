344cff26825828e90fbbfc849c83e2649c5da42e
Joel Carrizales <joelcarrizales22@gmail.com>
2022-08-23 16:21:24 -0500

    removing milestones

--- Seafall/Seafall/Controllers/GameController.cs
+++ Seafall/Seafall/Controllers/GameController.cs
@@ -50,10 +50,10 @@ public class GameController : ControllerBase
         milestones.Add(new Milestone("Clad in Marble", "Have five structures.", 4, "Available"));
         milestones.Add(new Milestone("Birth of an Empire", "Have three active colonies.", 4, "Available"));
         milestones.Add(new Milestone("Power of the Ancients", "Have two relics.", 3, "Available"));
-        milestones.Add(new Milestone("The Ancient Capital Razed", "Conquer the Ancient City of Ker.", 4, "Available"));
+        //milestones.Add(new Milestone("The Ancient Capital Razed", "Conquer the Ancient City of Ker.", 4, "Available"));
         milestones.Add(new Milestone("The Temple Falls", "Successfully raid the temple on Tortosa.", 5, "Available"));
         milestones.Add(new Milestone("The Temple Reborn", "Build the Temple on a build site you control.", 5, "Available"));
-        milestones.Add(new Milestone("Faces to the Whispers", "Have five advisors in your council room with your enmity on them.", 2, "Available"));
+        //milestones.Add(new Milestone("Faces to the Whispers", "Have five advisors in your council room with your enmity on them.", 2, "Available"));
         milestones.Add(new Milestone("The Secret of Bronze", "Discover the secret of the bronze statue.", 6, "Available"));
         milestones.Add(new Milestone("The Secret of Silver", "Discover the secret of the silver statue.", 6, "Available"));
         milestones.Add(new Milestone("The Secret of Gold", "Discover the secret of the gold statue.", 6, "Available"));
